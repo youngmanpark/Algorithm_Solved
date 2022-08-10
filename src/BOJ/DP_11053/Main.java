a8f4407ecc70f7f4653551855f59e0674f310ad2
YeongMin Park <py980627@skuniv.ac.kr>
2022-08-10 17:19:26 +0900

    new solved

---
+++ src/BOJ/DP_11053/Main.java
@@ -0,0 +1,83 @@
+package BOJ.DP_11053;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        FastReader fr = new FastReader();
+        int n = fr.nextInt();
+
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = fr.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            max = dp[i] > max ? dp[i] : max;
+        }
+        System.out.println(max);
+
+    }
+
+}
+
+class FastReader {
+
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public FastReader(String s) throws FileNotFoundException {
+        br = new BufferedReader(new FileReader(new File(s)));
+    }
+
+    String next() {
+        while (st == null || !st.hasMoreElements()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+        String str = "";
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+}
