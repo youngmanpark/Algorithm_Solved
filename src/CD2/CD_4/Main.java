01ef15c556d22a5b7c80cc5b2ea8166c442c6958
YeongMin Park <py980627@skuniv.ac.kr>
2023-01-04 17:08:04 +0900

    new commit

---
+++ src/CD2/CD_4/Main.java
@@ -0,0 +1,50 @@
+package CD2.CD_4;
+
+
+
+import java.util.*;
+
+class Solution{
+    public String solution (String []arr, int k){
+        String answer;
+
+        HashMap<String, HashSet<String>> voteHash = new HashMap<>();
+        HashMap<String,Integer> candidate = new HashMap<>();
+        HashMap<String, Integer> cntHash = new HashMap<>();
+        ArrayList<String > list = new ArrayList();
+
+        for (String x : arr) {
+            String a = x.split(" ")[0];
+            String b = x.split(" ")[1];
+            voteHash.putIfAbsent(a, new HashSet<String>());
+            voteHash.get(a).add(b);
+            candidate.put(b, candidate.getOrDefault(b,0)+1);
+        }
+        int max = 0;
+        for (String key : voteHash.keySet()) {
+            int cnt = 0;
+            for (String x : voteHash.get(key)) {
+                if(candidate.get(x)>=k)
+                    cnt++;
+                cntHash.put(key, cnt);
+                max = Math.max(max, cnt);
+            }
+        }
+        for (String key : cntHash.keySet()) {
+            if (cntHash.get(key) == max) {
+                list.add(key);
+            }
+        }
+        Collections.sort(list);
+        answer= list.get(0);
+        return answer;
+
+    }
+    public static void main(String []args){
+       Solution T = new Solution();
+        String [] arr  = {"john tom", "daniel luis", "john luis", "luis tom",
+                "daniel tom", "luis john"};
+        System.out.println(T.solution(arr,2));
+
+    }
+}
