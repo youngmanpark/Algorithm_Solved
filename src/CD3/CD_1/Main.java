81e8c8360af8773bebe8e1228e5a59b8f66fa7ee
YeongMin Park <py980627@skuniv.ac.kr>
2023-01-07 18:00:59 +0900

    new commit

--- src/CD3/CD_1/Main.java
+++ src/CD3/CD_1/Main.java
@@ -1,42 +1,31 @@
-/*
+
 package CD3.CD_1;
 
 import java.util.*;
 class Solution {
-    public int find1num(String str){
-        return str.length() - str.replace(String.valueOf(1), "").length();
-    }
+	public int[] solution(int[] nums){
+		int[] answer = new int[nums.length];
+		int[][] res = new int[nums.length][2];
+		for(int i = 0; i < nums.length; i++){
+			int cnt = 0;
+			int tmp = nums[i];
+			while(tmp > 0){ //2로 나누어 (2진수이니)0이될때까지  진행하면 나머지들의 개수가 곧 1의 개수
+				cnt += (tmp % 2);
+				tmp = tmp / 2;
+			}
+			res[i][0] = cnt;
+			res[i][1] = nums[i];
+		}
+		Arrays.sort(res, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+		for(int i = 0; i < res.length; i++){
+			answer[i] = res[i][1];
+		}
+		return answer;
+	}
 
-    public int[] solution(int[] nums){
-//        int[] answer = new int[nums.length];
-//
-//        TreeMap<Integer, Integer> map = new TreeMap<>();
-//
-//        for (int x : nums) {
-//            map.put(x, find1num(Integer.toBinaryString(x)));
-//        }
-//        ArrayList<Integer> list = new ArrayList<>();
-//        int max = 0;
-//        for (int i = 0; i<nums.length;i++) {
-//            list.add(map.get(i));
-//            int tmp;
+	public static void main(String[] args){
+		Solution T = new Solution();
 
-//
-//            if (max < map.get(i)) {
-//                max = map.get(i);
-//            }
-//
-//        }
-
-
-
-        return answer;
-    }
-
-    public static void main(String[] args){
-        Solution T = new Solution();
-        System.out.println(Arrays.toString(T.solution(new int[]{5, 6, 7, 8, 9})));
-        System.out.println(Arrays.toString(T.solution(new int[]{1, 1, 6, 2, 2, 7, 3, 14})));
-        System.out.println(Arrays.toString(T.solution(new int[]{14, 4, 2, 6, 3, 10, 10, 5, 5, 7, 7, 14})));
-    }
-}*/
+        System.out.println(Arrays.toString(T.solution(new int[]{5,6,7,8,9})));
+	}
+}
