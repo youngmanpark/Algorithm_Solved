8c2320d6950ce440bdcf9c14f9c753fcc243a9a7
YeongMin Park <py980627@skuniv.ac.kr>
2023-01-11 15:24:42 +0900

    new commit

--- src/CD5/Proc5_1.java
+++ src/CD5/Proc5_1.java
@@ -1,15 +1,28 @@
 package CD5;
 
 
-public class Proc5_1 {
-    public int solution(int []nums) {
-        int answer = 0;
+import java.util.HashMap;
 
+public class Proc5_1 {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int[] nH = new int[2];
+        int sum = 0;
+//        nH[0] = 1;
+        for (int x : nums) {
+            sum += x;
+            if (sum % 2 == 1) answer++;
+            if (sum % 2 == 1)
+                answer += nH[0];
+            else answer += nH[1];
+            nH[sum % 2] += 1;
+        }
         return answer;
 
     }
+
     public static void main(String[] args) {
         Proc5_1 T = new Proc5_1();
-        System.out.println(T.solution(new int []{1,3,5}));
+        System.out.println(T.solution(new int[]{1, 3, 5}));
     }
 }
--- src/CD5/Proc5_2.java
+++ src/CD5/Proc5_2.java
@@ -1,14 +1,49 @@
 package CD5;
 
-public class Proc5_2 {
-    public String[] solution(int []nums) {
-        String  []answer={} ;
+import javax.swing.*;
+import java.util.Arrays;
 
+public class Proc5_2 {
+    public String[] solution(int[] nums) {
+        String[] answer = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            int t = 1;
+            n--;
+            for (int j = 1; ; j++) {
+                if (n >= 9 * t)
+                    n = n - (9 * t);
+                else {
+                    int len = (j + 1) / 2;
+                    int[] res = new int[100];
+                    String pal = "";
+                    res[0] = 1;
+                    for (int k = 0; k < len; k++) {
+                        res[k] += (n / t);
+                        pal += String.valueOf(res[k]);
+                        n = n % t;
+                        t = t / 10;
+                    }
+
+                    for (int k = len - 1 - (j % 2); k >= 0; k--) {
+                        pal += String.valueOf(res[k]);
+                    }
+                    answer[i] = pal;
+                    break;
+
+                }
+                if (j % 2 == 0)
+                    t = t * 10;
+
+            }
+
+        }
         return answer;
 
     }
+
     public static void main(String[] args) {
         Proc5_2 T = new Proc5_2();
-        System.out.println(T.solution(new int[]{1, 12, 24}));
+        System.out.println(Arrays.toString(T.solution(new int[]{1, 12, 24})));
     }
 }
