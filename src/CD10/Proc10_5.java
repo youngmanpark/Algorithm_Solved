30ca17dcad42e1005ef0cdc6424010ea56a2d997
YeongMin Park <py980627@skuniv.ac.kr>
2023-01-25 17:01:15 +0900

    new commit

--- src/CD10/Proc10_5.java
+++ src/CD10/Proc10_5.java
@@ -1,8 +1,35 @@
 package CD10;
 
+import java.util.HashMap;
+
 public class Proc10_5 {
     public int solution(int[][]apples,int s ,int k){
         int answer=0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int[] apple : apples) {
+            map.put(apple[0], apple[1]);
+        }
+        int[] maxL=new int[k+1];
+        int[] maxR=new int[k+1];
+        maxL[0] = map.getOrDefault(s, 0);
+        maxR[0] = map.getOrDefault(s, 0);
+        answer = maxL[0];
+        for (int i = 1; i <= k; i++) {
+            if (s - i >= 0) {
+                maxL[i] = maxL[i - 1] + map.getOrDefault(s - i, 0);
+            } else {
+                maxL[i] = maxL[i - 1];
+            }
+            answer = Math.max(answer, maxL[i]);
+            maxR[i] = maxR[i - 1] + map.getOrDefault(s + i, 0);
+            answer = Math.max(answer, maxR[i]);
+        }
+        for (int j = 1; j < (k + 1) / 2; j++) {
+            answer = Math.max(answer, maxL[j] + maxR[k - 2 * j] - maxR[0]);
+            answer = Math.max(answer, maxR[j] + maxL[k - 2 * j] - maxR[0]);
+        }
+
+
         return answer;
     }
     public static void main(String[] args) {
